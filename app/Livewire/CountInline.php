6d4fa4ee69d8470c251831a40f5e61dde246efbb
rafael_pozzebon <rafael@unitronica.com.br>
2023-10-23 13:14:51 -0300

    meus primeiros components

---
+++ app/Livewire/CountInline.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CountInline extends Component
+{
+    public function render()
+    {
+        return <<<'HTML'
+        <div>
+            Return in one arquive.
+        </div>
+        HTML;
+    }
+}
